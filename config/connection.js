1b1efd5bdef718967cfa5c4a706f03627673f344
Dalto <dalton.earl@outlook.com>
2020-04-06 19:20:07 -0600

    server test

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,20 @@ var username = process.env.mySQL_username;
 var password = process.env.mySQL_password;
 
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Earl4370",
-  database: "burgers_db"
-});
+var connection; 
+
+if(process.env.JAWSDB_URL){
+  console.log("you do it?");
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  console.log("not quite!")
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: username,
+    password: password,
+    database: 'burgers_db'
+  })
+}
 
 // Make connection.
 connection.connect(function(err) {
--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE    burger
 (
     id int not null PRIMARY KEY auto_increment,
     burger_name varchar(65),
-    devoured BOOLEAN default false
+    devoured BOOLEAN default false,
+    createdAt TIMESTAMP NOT NULL DEFAULT current_timestamp
 
 );
